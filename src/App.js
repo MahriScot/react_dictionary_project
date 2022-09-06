973894501622f07f170745a15ad428878d6a79a4
MahriScot <mahristewart@hotmail.co.uk>
2022-09-06 14:50:09 +0100

    search engine set up

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import logo_no_name from "./logo_no_name.png";
+import logo_long_no_name from "./logo_long_no_name.png";
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo_no_name} className="img-fluid App-logo" alt="logo" />
-        Dictionary App by Mahri
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img
+            src={logo_long_no_name}
+            className="img-fluid App-logo"
+            alt="logo"
+          />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+      </div>
+      <footer>
+        <small>
+          This project is coded by Mahri Stewart and is open sourced on GitHub
+          and hosted on Netlify
+        </small>
+      </footer>
     </div>
   );
 }
