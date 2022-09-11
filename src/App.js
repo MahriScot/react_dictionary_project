74ff163d4af2a626ec80a68065c03590189ba447
MahriScot <mahristewart@hotmail.co.uk>
2022-09-11 13:05:55 +0100

    some general css (font etc) and footer links

--- src/App.js
+++ src/App.js
@@ -14,14 +14,38 @@ export default function App() {
           />
         </header>
         <main>
-          <Dictionary />
+          <h2 className="what_word">What word are you looking for?</h2>
+          <Dictionary className="Dictionary" />
         </main>
       </div>
       <footer>
-        <small>
-          This project is coded by Mahri Stewart and is open sourced on GitHub
-          and hosted on Netlify
-        </small>
+        <img src="images/terrain.png" alt="my_logo" width="20"></img>This
+        project is coded by{" "}
+        <a
+          href="https://steady-youtiao-11dde2.netlify.app"
+          target="_blank"
+          rel="noreferrer"
+          className="me"
+        >
+          Mahri Stewart
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/MahriScot/react_dictionary_project"
+          target="_blank"
+          rel="noreferrer"
+          className="github_and_netlify"
+        >
+          open sourced on GitHub
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://coruscating-starburst-e7d595.netlify.app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          hosted on Netlify
+        </a>
       </footer>
     </div>
   );
